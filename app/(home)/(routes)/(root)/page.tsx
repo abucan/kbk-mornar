8dba890175dc3adddfc6be7435a3bd0c09e41e73
Ante Bucan <ab8390@g.rit.edu>
2024-04-30 14:52:10 +0200

    first commit

---
+++ app/(home)/(routes)/(root)/page.tsx
@@ -0,0 +1,13 @@
+import { Banner } from '../_components/banner';
+import { Navbar } from '../_components/navbar';
+
+const RootPage = () => {
+  return (
+    <div>
+      <Banner />
+      <Navbar />
+    </div>
+  );
+};
+
+export default RootPage;
---
+++ app/(home)/(routes)/_components/banner.tsx
@@ -0,0 +1,11 @@
+export const Banner = () => {
+  return (
+    <div className='bg-indigo-600'>
+      <div className='max-w-screen-xl mx-auto px-4 py-3 text-white sm:text-center md:px-8'>
+        <p className='font-medium'>
+          KBK MORNAR SPLIT - GYM - SAUNA - MASSAGE - PRIVATE TRAININGS
+        </p>
+      </div>
+    </div>
+  );
+};
