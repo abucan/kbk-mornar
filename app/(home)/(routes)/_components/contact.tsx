8862202c5525602fdfa51e8c37e28dfb0b6cfedc
Ante Bucan <ab8390@g.rit.edu>
2024-05-17 12:53:09 +0200

    Trainers

--- app/(home)/(routes)/_components/contact.tsx
+++ app/(home)/(routes)/_components/contact.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { SendEmailForm } from '@/components/send-email-form';
-import { Map } from './map';
 import { Mail, MapPin, Phone } from 'lucide-react';
 import { Card } from '@/components/ui/card';
 
@@ -45,7 +44,11 @@ export const Contact = () => {
                 ))}
               </ul>
             </div>
-            <Map />
+            <iframe
+              src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5786.3183454074015!2d16.42338269439156!3d43.51987336637534!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x13355de5b75b811f%3A0xaf24fc6abf0e93b!2sPoljud%2C%20Split!5e0!3m2!1sen!2shr!4v1715941582052!5m2!1sen!2shr'
+              className='w-full h-[250px]'
+              loading='lazy'
+            ></iframe>
           </div>
           <div className='flex-1 sm:max-w-lg lg:max-w-md grid place-items-center'>
             <Card className='p-8 w-[450px]'>
--- app/(home)/(routes)/_components/trainers.tsx
+++ app/(home)/(routes)/_components/trainers.tsx
@@ -1,16 +1,7 @@
+import { trainers } from '@/utils/trainers';
 import { Header } from './header';
 import { ShapeDivider } from './shape-divider';
 import { TrainerItem } from './trainer-item';
-
-const team = [
-  {
-    avatar:
-      'https://images.unsplash.com/photo-1579017331263-ef82f0bbc748?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=685&q=80',
-    name: 'Martiana dialan',
-    title: 'Product designer',
-    desc: 'Lorem Ipsum is simply dummy text of the printing and typesettin industry.',
-  },
-];
 
 export const Trainers = () => {
   return (
@@ -22,7 +13,7 @@ export const Trainers = () => {
       <div className='max-w-screen-xl mx-auto px-4 md:px-8'>
         <div className='mt-12'>
           <ul className='grid gap-8 sm:grid-cols-2 md:grid-cols-3'>
-            {team.map((item, idx) => (
+            {trainers.map((item, idx) => (
               <TrainerItem {...item} key={idx} />
             ))}
           </ul>
