865aa73943b16d5faac6677ef47e144c84a9537b
Ante Bucan <ab8390@g.rit.edu>
2024-10-31 13:33:01 +0100

    db

--- lib/db.ts
+++ lib/db.ts
@@ -12,6 +12,9 @@ export const connectToDatabase = async () => {
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
+      ssl: {
+        rejectUnauthorized: true,
+      },
     });
 
     return connection;
