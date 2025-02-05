67b0f498b318e92260a5c7184a907853dac9cc77
Ante Bucan <ab8390@g.rit.edu>
2025-02-05 12:27:08 +0100

    updated trainers

--- app/(home)/(routes)/(root)/page.tsx
+++ app/(home)/(routes)/(root)/page.tsx
@@ -1,29 +1,21 @@
-import { Hero } from "../_components/hero";
-import { Blog } from "../_components/blog";
-import { Gallery } from "../_components/gallery";
-import { Contact } from "../_components/contact";
-import { AboutUs } from "../_components/about-us";
-import { Schedule } from "../_components/schedule";
-import { Trainers } from "../_components/trainers";
-import { MobileHero } from "../_components/mobile-hero";
-import { getTrainersFromDB } from "@/actions/trainers.actions";
-import { FeaturedFighter } from "../_components/featured-fighter";
+import { Hero } from '../_components/hero';
+import { Blog } from '../_components/blog';
+import { trainerItems } from '@/utils/trainers';
+import { Gallery } from '../_components/gallery';
+import { Contact } from '../_components/contact';
+import { AboutUs } from '../_components/about-us';
+import { Schedule } from '../_components/schedule';
+import { Trainers } from '../_components/trainers';
+import { MobileHero } from '../_components/mobile-hero';
+import { FeaturedFighter } from '../_components/featured-fighter';
 
 const RootPage = async () => {
-  let trainersFromDB = [];
-
-  try {
-    trainersFromDB = await getTrainersFromDB();
-  } catch (error) {
-    console.error("Error fetching trainers:", error);
-  }
-
   return (
     <>
       <Hero />
       <MobileHero />
       <AboutUs />
-      <Trainers trainers={trainersFromDB} />
+      <Trainers trainers={trainerItems} />
       <FeaturedFighter />
       <Blog />
       <Gallery />
