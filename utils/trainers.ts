bef405478becc582774f271540c5ef6cfb9fba7e
Ante Bucan <ab8390@g.rit.edu>
2025-02-10 15:21:23 +0100

    updated trainers

--- utils/trainers.ts
+++ utils/trainers.ts
@@ -3,37 +3,46 @@ export const trainerItems = [
     id: 1,
 
     avatar:
-      'https://cloud.appwrite.io/v1/storage/buckets/665d9a7b0036a3f8612f/files/67a2222f0026bf1b39da/view?project=665d75ee002274861442&mode=admin',
-    name: 'Dario Jurišić',
-    title: 'Trener',
+      "https://cloud.appwrite.io/v1/storage/buckets/665d9a7b0036a3f8612f/files/67aa073e0012ece5f429/view?project=665d75ee002274861442&mode=admin",
+    name: "Dario Jurišić",
+    title: "Trener",
     content:
-      'Dario Jurišić završio je Ekonomski fakultet, kao i stručno usavršavanje za trenera borilačkog sporta na Kineziološkom fakultetu u Zagrebu. U svijetu borilačkih sportova prisutan je od 1998. godine, gdje je prvo izgradio uspješnu natjecateljsku karijeru. Kao višestruki prvak Hrvatske i član nacionalne reprezentacije, postigao je zapažene rezultate na domaćoj i međunarodnoj sceni.\n\nNakon završetka natjecateljske karijere, Dario se posvetio trenerskom radu i stručnom razvoju kluba. Pod njegovim vodstvom klub je ostvario iznimne uspjehe, uključujući brojne medalje s europskih i svjetskih natjecanja, čime se pozicionirao među najuspješnije klubove u Hrvatskoj i šire.\n\nNjegova stručnost, iskustvo i posvećenost osiguravaju kvalitetan razvoj sportaša svih uzrasta, od početnika do vrhunskih natjecatelja. Dario je posvećen promociji sporta, timskog duha i vrhunskih rezultata, uvijek s ciljem podizanja standarda u borilačkom sportu.',
+      "Dario Jurišić završio je Ekonomski fakultet, kao i stručno usavršavanje za trenera borilačkog sporta na Kineziološkom fakultetu u Zagrebu. U svijetu borilačkih sportova prisutan je od 1998. godine, gdje je prvo izgradio uspješnu natjecateljsku karijeru. Kao višestruki prvak Hrvatske i član nacionalne reprezentacije, postigao je zapažene rezultate na domaćoj i međunarodnoj sceni.\n\nNakon završetka natjecateljske karijere, Dario se posvetio trenerskom radu i stručnom razvoju kluba. Pod njegovim vodstvom klub je ostvario iznimne uspjehe, uključujući brojne medalje s europskih i svjetskih natjecanja, čime se pozicionirao među najuspješnije klubove u Hrvatskoj i šire.\n\nNjegova stručnost, iskustvo i posvećenost osiguravaju kvalitetan razvoj sportaša svih uzrasta, od početnika do vrhunskih natjecatelja. Dario je posvećen promociji sporta, timskog duha i vrhunskih rezultata, uvijek s ciljem podizanja standarda u borilačkom sportu.",
   },
   {
     id: 2,
     avatar:
-      'https://cloud.appwrite.io/v1/storage/buckets/665d9a7b0036a3f8612f/files/6745c4fd00105519a31f/view?project=665d75ee002274861442&project=665d75ee002274861442&mode=admin',
-    name: 'Ivan Tabak',
-    title: 'Trener',
+      "https://cloud.appwrite.io/v1/storage/buckets/665d9a7b0036a3f8612f/files/67aa0b09000e1550f344/view?project=665d75ee002274861442&mode=admin",
+    name: "Ivan Tabak",
+    title: "Trener",
     content:
-      'Ivan Tabak\nRođen 26.09. 1992 u Splitu\nInstruktor fitnesa i trener u Kickboxing klubu Mornar\nBivši natjecatelj u kickboxingu i muay thaiu , a sada trener sa višegodišnjim iskustvom u radu sa rekreativcima i sportašima.',
+      "Ivan Tabak\nRođen 26.09. 1992 u Splitu\nInstruktor fitnesa i trener u Kickboxing klubu Mornar\nBivši natjecatelj u kickboxingu i muay thaiu , a sada trener sa višegodišnjim iskustvom u radu sa rekreativcima i sportašima.",
   },
   {
     id: 3,
     avatar:
-      'https://cloud.appwrite.io/v1/storage/buckets/665d9a7b0036a3f8612f/files/67a34a610003e3e2681c/view?project=665d75ee002274861442&mode=admin',
-    name: 'Petar Pastuović',
-    title: 'Trener',
+      "https://cloud.appwrite.io/v1/storage/buckets/665d9a7b0036a3f8612f/files/67a34a610003e3e2681c/view?project=665d75ee002274861442&mode=admin",
+    name: "Petar Pastuović",
+    title: "Trener",
     content:
-      'Petar Pastuović, trener koji je u kickboxing klub Mornar kročio još davne 2001.g i ta strast za borilačkim sportom traje sve do danas... pune 23 godine. Kao i 2001g tako i danas doprinosi našem klubu i mladim uzrastima gdje odgajanjem jedne sportske obitelji, natjecateljskog duha se stvaraju vrhunski natjecatelji zavidnih perfomansi s vrhunskim rezultatima. \nPrvih 6 godina je bio aktivni natjecatelj i hrvatski reprezentativac gdje iza sebe ima iskustvo na državnim, europskim i svjetskim prvenstvima uz popriličan broj zlatnih odličja, no životni put je prevladao, te je aktivno bavljenje sportom i natjecanjima zamijenio s obrazovanjem, gdje se uhvatio u koštac s istim. Nekadašnje izazove na tatamiju i ringu zamijenio je titulom inženjera građevine.\n\nSvoju ljubav i znanje prema borilačkom sportu je vrlo brzo počeo primjenjivati na slijedeće naraštaje i u ulozi trenera se prvi put nalazi 2006/2007g gdje još uvijek povremeno sudjelovao na natjecanjima, sve do 2010.g gdje se potpuno educirao i posvetio trenerskom svijetu.\n\n\nDanas, suprug i otac dvoje djece uvelike sudjeluje u procesu stvaranja vrhunskih sportaša koji pod bojama kbk kluba Mornar nižu uspijehe.',
+      "Petar Pastuović, trener koji je u kickboxing klub Mornar kročio još davne 2001.g i ta strast za borilačkim sportom traje sve do danas... pune 23 godine. Kao i 2001g tako i danas doprinosi našem klubu i mladim uzrastima gdje odgajanjem jedne sportske obitelji, natjecateljskog duha se stvaraju vrhunski natjecatelji zavidnih perfomansi s vrhunskim rezultatima. \nPrvih 6 godina je bio aktivni natjecatelj i hrvatski reprezentativac gdje iza sebe ima iskustvo na državnim, europskim i svjetskim prvenstvima uz popriličan broj zlatnih odličja, no životni put je prevladao, te je aktivno bavljenje sportom i natjecanjima zamijenio s obrazovanjem, gdje se uhvatio u koštac s istim. Nekadašnje izazove na tatamiju i ringu zamijenio je titulom inženjera građevine.\n\nSvoju ljubav i znanje prema borilačkom sportu je vrlo brzo počeo primjenjivati na slijedeće naraštaje i u ulozi trenera se prvi put nalazi 2006/2007g gdje još uvijek povremeno sudjelovao na natjecanjima, sve do 2010.g gdje se potpuno educirao i posvetio trenerskom svijetu.\n\n\nDanas, suprug i otac dvoje djece uvelike sudjeluje u procesu stvaranja vrhunskih sportaša koji pod bojama kbk kluba Mornar nižu uspijehe.",
   },
   {
     id: 4,
     avatar:
-      'https://cloud.appwrite.io/v1/storage/buckets/665d9a7b0036a3f8612f/files/6745c505000e862d7253/view?project=665d75ee002274861442&project=665d75ee002274861442&mode=admin',
-    name: 'Mate Grčić',
-    title: 'Trener',
+      "https://cloud.appwrite.io/v1/storage/buckets/665d9a7b0036a3f8612f/files/6745c505000e862d7253/view?project=665d75ee002274861442&project=665d75ee002274861442&mode=admin",
+    name: "Mate Grčić",
+    title: "Trener",
     content:
-      'Mate Grčić, rođen 2. siječnja 1994. u Splitu, magistar je kineziologije i trener Boćarskog kluba osoba s invaliditetom Sveti Duje. Također je predsjednik kluba i dopredsjednik Hrvatskog Saveza boćanja osoba s invaliditetom. Ima višegodišnje iskustvo u radu s djecom i sportašima s invaliditetom te sudjeluje u projektima Športskog Saveza invalida grada Splita i Hrvatskog paraolimpijskog odbora.',
+      "Mate Grčić, rođen 2. siječnja 1994. u Splitu, magistar je kineziologije i trener Boćarskog kluba osoba s invaliditetom Sveti Duje. Također je predsjednik kluba i dopredsjednik Hrvatskog Saveza boćanja osoba s invaliditetom. Ima višegodišnje iskustvo u radu s djecom i sportašima s invaliditetom te sudjeluje u projektima Športskog Saveza invalida grada Splita i Hrvatskog paraolimpijskog odbora.",
+  },
+  {
+    id: 5,
+    avatar:
+      "https://cloud.appwrite.io/v1/storage/buckets/665d9a7b0036a3f8612f/files/67aa07fe000ef375c0df/view?project=665d75ee002274861442&mode=admin",
+    name: "Anton Žanko",
+    title: "Trener",
+    content:
+      "Anton Žanko, student 3. godine Kineziološkog fakulteta (smjer kineziterapija), donosi svoje znanje i iskustvo u kickboxing klub Mornar, pomažući vam kroz:\n\nSportske i terapeutske masaže – za opuštanje, bolju cirkulaciju i brži oporavak\nFunkcionalne treninge – usmjerene na poboljšanje pokretljivosti, snage i prevenciju ozljeda\n\nUz razumijevanje pokreta i rehabilitacije, Anton kroz individualan pristup prilagođava masaže i treninge vašim potrebama – bilo da se želite osloboditi napetosti, ubrzati oporavak ili poboljšati tjelesnu funkcionalnost.",
   },
 ];
