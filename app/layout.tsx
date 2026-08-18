76b3a92ca61496f8772080f1d73584cb1a9078b6
Ante Bucan <ab8390@g.rit.edu>
2026-08-18 13:01:14 +0200

    Add Google Tag Manager script for enhanced tracking in RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
+      <head>
+        <script
+          async
+          src='https://www.googletagmanager.com/gtag/js?id=AW-18303407803'
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'AW-18303407803');`,
+          }}
+        />
+      </head>
       <body className={inter.className}>
         {children}
         <Analytics />
